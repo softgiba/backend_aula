666378bd67ff5c519fcb573bae87ce244b2639f7
Gilberto <37663177+softgiba@users.noreply.github.com>
2022-08-17 03:50:36 -0400

    Delete dbConect.js

--- v5/src/config/dbConect.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose"
-
-mongoose.connect("mongodb+srv://admin:CpoqwOeste@cluster0aula.traeq.mongodb.net/UserManager");
-
-let db = mongoose.connection;
-
-export default db;
