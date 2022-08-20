07b011a43d48eacb36217b38573209b28e8a6ac7
Gilberto Silva <faflecomgilberto@gmail.com>
2022-08-20 16:04:45 -0400

    Add node_modules

--- v5/server.js
+++ v5/server.js
@@ -4,7 +4,7 @@ import swaggerUi from 'swagger-ui-express';
 // definição de porta condicional do proxy ou na 3000
 const port = process.env.PORT || 3030; 
 
-app.use('/swagger', swaggerUi.serve, swaggerUi.setup(null, { swaggerOptions: { url: './docs/swagger.json' } }));
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(null, { swaggerOptions: { url: './docs/swagger.jcdson' } }));
 
 
 // retorno no terminal com o link
