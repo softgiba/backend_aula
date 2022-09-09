50a94a547e76fe85f38f312199f7446abe6db04a
Gilberto <gilberto.silva@ifro.edu.br>
2022-09-09 03:37:23 +0000

    updanting

--- v6/src/controllers/RotaController.js
+++ v6/src/controllers/RotaController.js
@@ -1,27 +1,27 @@
 import rotas from "../models/Rota.js";
 
-class RotaController {
-  static listarRotas = async (req, res) => {
-    try {
-      const nome = req.query.nome;
-      const { page, perPage } = req.query;
-      const options = { // limitar a quantidade máxima por requisição
-        nome: (nome),
-        page: parseInt(page) || 1,
-        limit: parseInt(perPage) || 10,
-      };
-      if (!nome) {
-        const rota = await rotas.paginate({}, options);
-        return res.json(rota);
-      } else {
-        const rota = await rotas.paginate({ nome }, options);
-        return res.json(rota);
-      }
-    } catch (err) {
-      console.error(err);
-      return res.status(500).send(err);
-    }
-  }
+
+
+  //   try {
+  //     const nome = req.query.nome;
+  //     const { page, perPage } = req.query;
+  //     const options = { // limitar a quantidade máxima por requisição
+  //       nome: (nome),
+  //       page: parseInt(page) || 1,
+  //       limit: parseInt(perPage) || 10,
+  //     };
+  //     if (!nome) {
+  //       const rota = await rotas.paginate({}, options);
+  //       return res.json(rota);
+  //     } else {
+  //       const rota = await rotas.paginate({ nome }, options);
+  //       return res.json(rota);
+  //     }
+  //   } catch (err) {
+  //     console.error(err);
+  //     return res.status(500).send(err);
+  //   }
+  // }
 
 
   static listarRotaPorId = async (req, res) => {
